ad725374c5dce3d3dc4d8bc41d0a6af663401f01
Tobias S. Sørensen <Tss3331@hotmail.com>
2017-09-13 12:27:11 +0200

    Lommeregner Client

--- LommeregnerClient/Program.cs
+++ LommeregnerClient/Program.cs
@@ -10,6 +10,17 @@ namespace LommeregnerClient
 	{
 		static void Main(string[] args)
 		{
+			using(var Client = new ServiceReference1.Service1Client())
+			{
+				Console.WriteLine("5+5= " + Client.Plus(5, 5));
+				Console.WriteLine("5-5= " + Client.Minus(5, 5));
+				Console.WriteLine("5*5= " + Client.Gange(5, 5));
+				Console.WriteLine("5/5= " + Client.Divider(5, 5));
+
+				Console.WriteLine("All in one method: " + Client.All(35, 4));
+
+				Console.ReadLine();
+			}
 		}
 	}
 }
